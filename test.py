440344bb22a2b8db16bf7d8a3c22c5d076b7e480
OkayYashvi <yashvipatel1183@gmail.com>
2025-01-16 15:39:58 +0530

    basic structure

---
+++ test.py
@@ -0,0 +1,9 @@
+'''
+This is my own work as defined by the SAIBT Academic Integrity Policy.
+I am fully aware of the consequences of academic misconduct as defined
+by the SAIBT Academic Integrity Policy.
+
+SAIBT ID:   43173
+Name:       Yashvi Chetankumar Patel
+Date:       13/01/2025
+'''
